a23fc01a94ee97307f073f960267e3428960a230
Chamika Dilshan <chamikadilshan1123@gmail.com>
2025-04-12 00:13:39 +0530

    change firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7iyY1QfXM4NXtAlYJ5_XPCpivaxc7HVA",
-  authDomain: "estatecraft-housing.firebaseapp.com",
-  projectId: "estatecraft-housing",
-  storageBucket: "estatecraft-housing.appspot.com",
-  messagingSenderId: "612483716642",
-  appId: "1:612483716642:web:c48a7c7e3f12f8e7f242e0"
+  apiKey: "AIzaSyCrJQEmrPpqVFQ1nsuPX6soXakCwTN2Bm8",
+  authDomain: "project9-76d8a.firebaseapp.com",
+  projectId: "project9-76d8a",
+  storageBucket: "project9-76d8a.firebasestorage.app",
+  messagingSenderId: "621431377723",
+  appId: "1:621431377723:web:d861a67ab881a4aed5311e"
 };
 
 // Initialize Firebase
