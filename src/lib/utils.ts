a3d346f4c4e3f5412af059749656a7ba29b89b2b
Chamika Dilshan <it22118318@my.sliit.lk>
2025-04-22 10:58:55 +0530

    change currency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'INR',
+    currency: 'LKR',
     maximumFractionDigits: 0
   }).format(amount)
 }
